b4c5c8bc1ba94375a4b3b131e24263dcf0c1baf0
Gerarvvdor <00053520@uca.edu.sv>
2024-05-05 10:52:45 -0600

    commit porque si

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,5 @@
 from math import *
+
 numbers = [] #Lista que guarda el número inicial
 answer = ""
 number_string = input("Ingrese los números que desea para calcular la raiz: ")
@@ -35,7 +36,7 @@ operacion_actual = int(paired_numbers[0])
 print("Primera operación:", operacion_actual)
 
 for i in range(1,10):
-    if (i*i) < operacion_actual:
+    if (i*i) <= operacion_actual:
         actual_square = i
         actual_cuad = i*i
   
@@ -48,26 +49,28 @@ answer = str(actual_square)
 paired_numbers.pop(0)
 
 # Bucle para elementos del 2 hasta n
-def square_method():
-    while len(paired_numbers) > 0:
-        operacion_actual = str(operacion_actual) + (paired_numbers[0])
-        print("Resta actual: ", operacion_actual)
-        operacion_actual = int(operacion_actual)
-        num_div = operacion_actual
-        num_div = str(num_div)
-        num_div = num_div[:-1]
-        num_div = int(num_div)
-        print("Numero sin el último",num_div)
-        residuo = floor(num_div/(2*int(answer)))
-        if residuo > 10:
-            residuo = 9
-        else:
-            residuo
-        print("Residuo: ", residuo)
-        producto = num_div * residuo
+while len(paired_numbers) > 0:
+    operacion_actual = str(operacion_actual) + (paired_numbers[0])
+    print("Resta actual: ", operacion_actual)
+    operacion_actual = int(operacion_actual)
+    num_div = operacion_actual
+    num_div = str(num_div)
+    num_div = num_div[:-1]
+    num_div = int(num_div)
+    print("Numero sin el último",num_div)
+    residuo = floor(num_div/(2*int(answer)))
+    if residuo > 10:
+        residuo = 9
+    else:
+        residuo
+    print("Residuo: ", residuo)
+    producto = num_div * residuo
 
-        operacion_actual = operacion_actual - producto
-        print("Después de resta: ",operacion_actual)
-        paired_numbers.pop(0)
+    operacion_actual = operacion_actual - producto
+    print("Después de resta: ",operacion_actual)
+    paired_numbers.pop(0)
+    answer = answer + str(residuo)
 
+
+print("La raiz es: ", answer)
 
