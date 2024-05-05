01f40bd77b0d20b35914a6f6d8d3ebfc29d110c0
GerarVv-dor <ggvvdor@gmail.com>
2024-05-05 10:48:40 -0600

    Adding comments

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,4 @@
+from math import floor
 from math import *
 numbers = [] #Lista que guarda el número inicial
 answer = ""
@@ -35,7 +36,7 @@ operacion_actual = int(paired_numbers[0])
 print("Primera operación:", operacion_actual)
 
 for i in range(1,10):
-    if (i*i) < operacion_actual:
+    if (i*i) <= operacion_actual:
         actual_square = i
         actual_cuad = i*i
   
@@ -47,27 +48,34 @@ answer = str(actual_square)
 # Eliminar el primer elemento de paired_numbers
 paired_numbers.pop(0)
 
-# Bucle para elementos del 2 hasta n
-def square_method():
-    while len(paired_numbers) > 0:
-        operacion_actual = str(operacion_actual) + (paired_numbers[0])
-        print("Resta actual: ", operacion_actual)
-        operacion_actual = int(operacion_actual)
-        num_div = operacion_actual
-        num_div = str(num_div)
-        num_div = num_div[:-1]
-        num_div = int(num_div)
-        print("Numero sin el último",num_div)
-        residuo = floor(num_div/(2*int(answer)))
-        if residuo > 10:
-            residuo = 9
-        else:
-            residuo
-        print("Residuo: ", residuo)
-        producto = num_div * residuo
+# Bucle para elementos del array del 01 hasta n
+while len(paired_numbers) > 0:
+    operacion_actual = str(operacion_actual) + (paired_numbers[0]) #se guarda la resta junto con el siguiente par de números
+    print("Resta actual: ", operacion_actual)
+    operacion_actual = int(operacion_actual) #se convierte a entero
+    num_div = operacion_actual #se guarda el número actual para quitarle el último dígito
+    num_div = str(num_div)
+    num_div = num_div[:-1] #se quita el último dígito
+    num_div = int(num_div) #se convierte a entero para poder operar con él
+    print("Numero sin el último",num_div)
+    residuo = floor(num_div/(2*int(answer))) #se divide el número sin el último dígito entre el doble de la respuesta actual
+    if residuo > 10:
+        residuo = 9
+    else:
+        residuo
 
-        operacion_actual = operacion_actual - producto
-        print("Después de resta: ",operacion_actual)
-        paired_numbers.pop(0)
+    print("Residuo: ", residuo)
+    temp_ans = str(2*answer) + str(residuo) #Concatenando el doble de la raiz actual al residuo
+    print("Numero actual a multiplicar el residuo: ", temp_ans)
+    operacion_actual = int(operacion_actual)  #convirtiendo a entero el numero a dividor
+    print("Número con residuo: ", operacion_actual)
+    producto = int(temp_ans) * residuo #se multiplica el doble de la respuesta actual por el residuo
+
+    operacion_actual -= producto
+    print("Después de resta: ",operacion_actual)
+    paired_numbers.pop(0)
+    answer += str(residuo)
+
+print("Respuesta final:", answer)
 
 
